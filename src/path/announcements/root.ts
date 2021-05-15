3f8cca8c5e2f70bc69a2063f08fc28477e8e8e8f
Yi-Jyun Pan <pan93412@gmail.com>
2021-05-15 18:05:42 +0800

    fix(path/announcements/root): compatible with browsers (CORS)

--- src/path/announcements/root.ts
+++ src/path/announcements/root.ts
@@ -1,3 +1,3 @@
-const announcementRoot = "https://github.com/smhs-os-project/smhs-announcements/blob/master";
+const announcementRoot = "https://raw.githubusercontent.com/smhs-os-project/smhs-announcements/master";
 export const AnnouncementEndpoint = (toCombine: string) =>
   `${announcementRoot}/${toCombine.replace(/^\//, "")}?raw=true`;
