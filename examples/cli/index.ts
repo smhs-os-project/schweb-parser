daceb2fb6df0f45f24ec8ba10ca16146e205f9eb
Yi-Jyun Pan <pan93412@gmail.com>
2021-05-15 16:37:28 +0800

    feat(examples/cli): update the example

--- examples/cli/index.ts
+++ examples/cli/index.ts
@@ -1,16 +1,7 @@
 import yargs from "yargs";
 import { AnnouncementContentFormatter, AnnouncementFormatter } from "./formatter/announcement";
 import { StdpkgFormatter } from "./formatter/stdpkg";
-import { GetJson } from "../../src/http/get_json";
-import { GetAnnouncement, ListAnnouncements } from "../../src/path/announcements";
-import {
-  Announcement,
-  AnnouncementContentTypeChecker,
-  AnnouncementTypeChecker,
-} from "../../src/types/announcements/types";
-import {
-  StandardPackageTypeCheckerWrapper,
-} from "../../src/types/stdpkg/types";
+import { ListAnnouncements, GetAnnouncement } from "../..";
 
 yargs
   .scriptName("schweb-parser-cli")
@@ -25,16 +16,7 @@ yargs
     },
     async (args) => {
       const category = args["category"] as string;
-      const response = await GetJson(
-        ListAnnouncements(category),
-        StandardPackageTypeCheckerWrapper(
-          (val: unknown): val is Announcement[] => {
-            return Array.isArray(val)
-              ? !val.map(AnnouncementTypeChecker).includes(false)
-              : false;
-          }
-        )
-      );
+      const response = await ListAnnouncements(category);
 
       if (response) {
         console.log(StdpkgFormatter(response, (data: any[]) => data.map(AnnouncementFormatter).join("\n\n")));
@@ -59,10 +41,7 @@ yargs
     async (args) => {
       const category = args["category"] as string;
       const id = args["id"] as string;
-      const response = await GetJson(
-        GetAnnouncement(category, id),
-        StandardPackageTypeCheckerWrapper(AnnouncementContentTypeChecker),
-      );
+      const response = await GetAnnouncement(category, id);
 
       if (response) {
         console.log(StdpkgFormatter(response, AnnouncementContentFormatter));
